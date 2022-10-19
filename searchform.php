f5c3884ac4b20abcfd287072f7a9ebafc5c7bad4
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-10-19 12:09:03 +0200

    improved accessibility of search form

--- searchform.php
+++ searchform.php
@@ -15,8 +15,7 @@
 			value="<?php echo get_search_query(); ?>" name="s"
 			title="<?php echo esc_attr_x( 'Search for:', 'label', 'chronus' ); ?>" />
 	</label>
-	<button type="submit" class="search-submit">
+	<button type="submit" class="search-submit" aria-label="<?php echo esc_attr_x( 'Search', 'submit button', 'chronus' ); ?>">
 		<?php echo chronus_get_svg( 'search' ); ?>
-		<span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'chronus' ); ?></span>
 	</button>
 </form>
